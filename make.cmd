707033903fea36ce81b661972a48b73fe6e66209
Olivier Lefebvre <olivier.lefebvre@live.com>
2017-04-15 14:05:58 +0200

    add make file

--- make.cmd
+++ make.cmd
@@ -1,3 +1,5 @@
 del /S /Q template\content\AngularSpa\node_modules\*.*
 del /S /Q template\content\Server\bin\*.*
 del /S /Q template\content\Server\obj\*.*
+dotnet restore build\build.csproj
+dotnet build build\build.csproj -f netcoreapp1.1
